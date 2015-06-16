c6b55c2b2814271a9930061ea56c27be739454a8
allanmc <allanmc@gmail.com>
2015-06-16 15:03:30 +0100

    Made startonde script executable

--- startnode.sh
+++ startnode.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -e PROXY_GROUP=devday --link dockerconf_mongo_1:db splinecl/developer-day
+docker run -e PROXY_GROUP=devday --link dockerconf_mongo_1:db -d splinecl/developer-day 
